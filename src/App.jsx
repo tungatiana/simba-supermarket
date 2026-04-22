c6881abf77687cf51d95c60de679192b7e01cd40
T1anarr <tiana.tunga@gmail.com>
2026-04-22 05:36:36 +0200

    Updated file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div>
-      <h1>Karibu Simba Supermarket!!</h1>
+      <h1>Karibu Simba Supermarket!</h1>
       <p>Welcome to my first React app built with Vite.</p>
     </div>
   )
