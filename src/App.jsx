e25a7c9baaba66a6cf18eaa6e699b4642d7ced91
T1anarr <tiana.tunga@gmail.com>
2026-04-22 05:16:08 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+function App() {
+  return (
+    <div>
+      <h1>Karibu Simba Supermarket!</h1>
+      <p>Welcome to my first React app built with Vite.</p>
+    </div>
+  )
+}
+
+export default App
