97fcffb7812d7bd9f3e9b813e651fb3e51474f5a
T1anarr <tiana.tunga@gmail.com>
2026-04-22 14:43:51 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 
-function App() {
+function Ap() {
   return (
     <>
       <Header />
