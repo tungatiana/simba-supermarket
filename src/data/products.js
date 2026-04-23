c5ff526da9997af2d2aecdad89d7beed285a4a73
T1anarr <tiana.tunga@gmail.com>
2026-04-23 03:06:38 +0200

    Updated product.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,16 +1,9 @@
-const products = [
-  {
-    id: 1,
-    name: "Milk",
-    price: 1200,
-    image: "https://via.placeholder.com/150"
-  },
-  {
-    id: 2,
-    name: "Bread",
-    price: 800,
-    image: "https://via.placeholder.com/150"
-  }
-];
+export const PRODUCTS = [
+  { id: 1, name: "Akabanga Hot Sauce",    priceRwf: 1500, image: "/img/akabanga.jpg",  inStock: true  },
+  { id: 2, name: "Inyange Milk 1L",       priceRwf: 1200, image: "/img/milk.jpg",      inStock: true  },
+  { id: 3, name: "Bralirwa Mutzig 50cl",  priceRwf: 1800, image: "/img/mutzig.jpg",    inStock: false },
+  { id: 4, name: "Azam Sugar 1kg",        priceRwf:  900, image: "/img/sugar.jpg",     inStock: true  },
+  { id: 5, name: "Dembe Maize Flour 5kg", priceRwf: 4500, image: "/img/flour.jpg",     inStock: true  },
+  { id: 6, name: "Tropical Pineapple",    priceRwf:  800, image: "/img/pineapple.jpg", inStock: true  },
 
-export default products;
+];
