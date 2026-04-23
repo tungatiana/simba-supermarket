0ac54d843b8ccfb822694e25b30d7631f44b828f
T1anarr <tiana.tunga@gmail.com>
2026-04-23 03:07:54 +0200

    Updated ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,17 @@
 export default function ProductCard({ product }) {
-  const { name, price, image } = product;
+  const { name, priceRwf, image, inStock } = product;
 
   const formattedPrice = new Intl.NumberFormat("en-RW", {
     style: "currency",
     currency: "RWF"
-  }).format(price);
+  }).format(priceRwf);
 
   return (
     <div className="product-card">
       <img src={image} alt={name} />
       <h3>{name}</h3>
       <p>{formattedPrice}</p>
+      {!inStock && <p style={{ color: "red" }}>Out of stock</p>}
     </div>
   );
 }
