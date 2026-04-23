a1ae1027e8823792391fcecfa8c601ebcc97d22e
T1anarr <tiana.tunga@gmail.com>
2026-04-23 02:46:58 +0200

    Added product.js in data

---
+++ src/data/products.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    id: 1,
+    name: "Milk",
+    price: 1200,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 2,
+    name: "Bread",
+    price: 800,
+    image: "https://via.placeholder.com/150"
+  }
+];
+
+export default products;
